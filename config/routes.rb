3dff4fe80d40edad3cacb43922351fed5866906e
Bunny <bunny@bunnys-macbook-pro.local>
2025-11-30 01:25:24 +0500

    cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => "/sidekiq"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Return Requests - API and UI
@@ -16,8 +14,6 @@ Rails.application.routes.draw do
 
   # Rules
   resources :rules, only: [:index]
-
-  mount Sidekiq::Web => "/sidekiq"
 
   # Root path
   root 'return_requests#index'
